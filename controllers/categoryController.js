85baa6425590059e2e6f0e53f20bd57a61f7527d
Abhishek  Srivastav <162480814+abhishekkrsrivastav@users.noreply.github.com>
2025-04-15 16:28:31 +0530

    apis for category section

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,12 +1,62 @@
 import db from '../config/db.js';
 
-
-export const getCategorie = async (req, res) => {
+// display all data of category
+export const getCategory = async (req, res) => {
     try {
-        const [rows] = await db.query('select * from parent_category');
+        const [rows] = await db.query(`SELECT * FROM parent_category ORDER BY sort_order ASC`);
         res.json(rows);
     } catch (error) {
         console.error('Database Error:', error);
         res.status(500).json({ error: 'Database error' });
     }
 }
+
+// add category
+
+export const addCategory = async (req, res) => {
+    const { category_name, sub_category, sub_sub_category, sort_order, status_id, category_url, parent_category_img_url } = req.body;
+
+    try {
+        const [result] = await db.query(`INSERT INTO parent_category 
+                (category_name, sub_category, sub_sub_category, sort_order, status_id, category_url, parent_category_img_url)
+             VALUES (?, ?, ?, ?, ?, ?, ?)`, [category_name, sub_category, sub_sub_category, sort_order, status_id, category_url, parent_category_img_url]);
+
+        res.json({
+            message: 'Category added successfully',
+            category_id: result.insertId
+        });
+    } catch (error) {
+        console.error('Database Error:', error);
+        res.status(500).json({ error: 'Database error' });
+    }
+};
+
+// upadte category
+export const updateCategory = async (req, res) => {
+    const { category_id, category_name, sub_category, sub_sub_category, sort_order, status_id, category_url, parent_category_img_url } = req.body;
+
+    try {
+        const [result] = await db.query(`UPDATE parent_category SET 
+                category_name = ?, sub_category = ?, sub_sub_category = ?,  sort_order = ?, status_id = ?, category_url = ?, parent_category_img_url = ?  WHERE category_id = ?`,
+            [category_name, sub_category, sub_sub_category, sort_order, status_id, category_url, parent_category_img_url, category_id]);
+
+        res.json({ message: "Category updated successfully" });
+    } catch (error) {
+        console.error("Database Error:", error);
+        res.status(500).json({ error: "Failed to update category" });
+    }
+};
+
+// delete category
+export const deleteCategory = async (req, res) => {
+    const { category_id } = req.params;
+
+    try {
+        const [result] = await db.query(`DELETE FROM parent_category WHERE category_id = ?`, [category_id]);
+
+        res.json({ message: "Category deleted successfully" });
+    } catch (error) {
+        console.error("Database Error:", error);
+        res.status(500).json({ error: "Failed to delete category" });
+    }
+};
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import { getCategorie } from '../controllers/categoryController.js';
+import { addCategory, deleteCategory, getCategory, updateCategory } from '../controllers/categoryController.js';
+import { verifyToken } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
-router.get("/get-categories", getCategorie);
+router.get("/get-categories", getCategory);
+router.post("/add-category", verifyToken, addCategory);
+router.put("/update-category", verifyToken, updateCategory);
+router.delete("/delete-category", verifyToken, deleteCategory)
 
 
 export default router;
